274585129e2e990cb1784469ea3aeb4948e54a03
Ilir  Hushi <ilirhushi@gmail.com>
2017-10-17 12:21:50 +0200

    FluentClient.php minor fix
    At function hydrateEntity, the $result param type is array but it is accessed as an object

--- src/Storage/FluentClient.php
+++ src/Storage/FluentClient.php
@@ -155,10 +155,10 @@ class FluentClient extends FluentAdapter implements ClientInterface
     {
         $client = new ClientEntity($this->getServer());
         $client->hydrate([
-            'id' => $result->id,
-            'name' => $result->name,
-            'secret' => $result->secret,
-            'redirectUri' => (isset($result->redirect_uri) ? $result->redirect_uri : null)
+            'id' => $result['id'],
+            'name' => $result['name'],
+            'secret' => $result['secret'],
+            'redirectUri' => (isset($result['redirect_uri']) ? $result['redirect_uri'] : null)
         ]);
         return $client;
     }
